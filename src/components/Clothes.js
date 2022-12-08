f3a6ca673378ae55e6d4a41fcb54d9e54b1fd716
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-12-08 12:16:34 -0600

    made the basic code structure for components and app.js

---
+++ src/components/Clothes.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+const Clothes = () => {
+
+    return (
+        <>
+            <h1>clothes</h1>
+        </>
+    )
+}
+
+export default Clothes
---
+++ src/components/Quiz.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+const Quiz = () => {
+
+    return (
+        <>
+            <h1>quiz</h1>
+        </>
+    )
+}
+
+export default Quiz
