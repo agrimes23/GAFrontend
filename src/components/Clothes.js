61e32bbf9fb30ccfcfe3c2c7e62746c906e2b54d
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-12-12 12:12:37 -0600

    tried a variety of ways to get the add to cart to work, just committing the work I did so far

--- src/components/Clothes.js
+++ src/components/Clothes.js
@@ -3,36 +3,54 @@ import {useState} from 'react'
 
 const Clothes = (props) => {
 
-    //FIXME: Only need a handle submit?? 
-    // const handleChange = (event) => {
-    //     setTest({...props.clothes[0].cart, [event.target.name]: event.target.value})
-    // }
-    // const handleSubmit = (event) => {
-    //     event.preventDefault()
+    // this will allow changes to the cart
+    const [cart, setCart] = useState([...props.userInfo.cart])
+    const [item, setItem] = useState({...props.item})
+    // const [chosenColor, setChosenColor] = useState()
+    // const [chosenHex, setChosenHex] = useState()
+    // const [getIndex, setGetIndex] = useState(0)
+    // const [index, setIndex] = useState(0)
 
-    //     props.handleEdit(animal)
-    // }
 
-    
+// // TODO: do this after successfully added to cart. Need to put onClick={handleClick} on div
+//     const handleClick = (e) => {
+//         setGetIndex(index)
+//         setChosenColor({...props.item.articleColorNames, [e.target.name]: props.item.articleColorNames[index]})
+//         setChosenHex({...props.item.rgbColors, [e.target.name]: e.target.value})
+//     }
+
+
+// Just want to submit whatever item was clicked on to cart array
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      setCart(...props.userInfo.cart, item)
+      console.log(cart)
+    //   props.handleAddToCart(cart)
+    }
+
+
 
     return (
         <>
+        
             <div className="card clothes-card m-1">
+            <form onSubmit={handleSubmit}>
                 <img src={props.item.images[0].url} className="card-img-top clothing-img" />
                 <div className="card-body">
-                <h4 className="card-title"><strong>{props.item.name}</strong></h4>
-                <h5 className="card-text">{props.item.price.formattedValue}</h5>
+                <h4 className="card-title" name="name"><strong>{props.item.name}</strong></h4>
+                <h5 className="card-text" name="price">{props.item.price.formattedValue}</h5>
                 <div className="d-inline-block">
                     {props.item.rgbColors.map((hexColor) => {
                         return (
                         <>
-                            <div className="clothing-color d-inline-block m-2" style={{background: hexColor}}  ></div>
+                            <div className="clothing-color d-inline-block m-2" name="rgbColors" style={{background: hexColor}}></div>
                         </>
                         )
                     })}
                 </div>
-                <button className="btn btn-primary d-block">Add to Cart</button> 
+                <input type="submit" className="btn btn-primary d-block" value="Add to Cart"/> 
                 </div>           
+            </form>
             </div>
         </>
     )
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,9 @@ import axios from 'axios'
 
 const Dashboard = (props) => {
 
-    const arr = props.item.map((it, i) => <Clothes item={it} key={i}/>)
+    const arr = props.item.map((it, i) => <Clothes item={it} key={i} userInfo={props.userInfo} handleAddToCart={props.handleAddToCart}/>)
+
+console.log(props.userInfo)
     return (
         <>
             <div className="container-fluid">
