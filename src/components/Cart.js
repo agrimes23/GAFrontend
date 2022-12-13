c5b721c729ccb942aaeaa0e3a8a396e99f4714f8
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-12-13 09:29:41 -0600

    was able to get the order summary equations to work!

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -39,15 +39,23 @@ const Cart = (props) => {
         (event.target.value === "+") ? setItemNum(itemNum + 1) : setItemNum(itemNum - 1)
     }
 
-    // const calculateTotal = (cartItem) => {
-    //                 {setPriceTotal(priceTotal + (Number(cartItem.price.formattedValue.split(" ")[1])))}
-    //                 {console.log("Price: " + cartItem.price.formattedValue)}
-                    
-    // }
 
     const calculateTax = () => {
-        setTotalTax(((priceTotal + 12) * 0.625).toFixed(2))
+        setTotalTax(((priceTotal + 12) * 0.0625).toFixed(2))
+        console.log("subtotal" + priceTotal)
+        console.log("est tax" + totalTax)
     }
+
+    useEffect (() => {
+        let tempPriceTotal = 0
+        props.userInfo.cart.forEach((cartItem)=> { 
+            tempPriceTotal += (Number(cartItem.price.formattedValue.split(" ")[1]))
+        })
+
+        setPriceTotal(tempPriceTotal)
+
+        calculateTax()
+    }, [])
 
     return (
         <>
@@ -60,13 +68,14 @@ const Cart = (props) => {
                      return (
                         
                       <>
+
                         <div className="flex-row card p-5 cart-card m-5">
                                 <img src={cartItem.images[0].url} className="cart-img float-left"></img>
                             <div className="row d-flex cart-info my-auto m-4">
                                 <h4 className=""><strong>{cartItem.name}</strong></h4>
                                 <h4 className=""><strong>Name</strong></h4>
                                 <h5 className="">Price: {cartItem.price.formattedValue}</h5>
-                               
+
                                 
                                 <h5 className="">Color: {cartItem.articleColorNames}</h5>
                                 <div className="d-flex">
@@ -74,8 +83,8 @@ const Cart = (props) => {
                                     <button className="btn btn-dark rounded-circle my-3 mx-2 pb-2" value="+" onClick={adjustItemNum}>+</button>
                                     <button className="btn btn-dark rounded-circle my-3 mx-2 px-3 pb-2" value="-">-</button>
                                 </div>
-                                {console.log(cartItem)}
-                                <button className="btn btn-danger mt-4 mx-2 w-50" onClick={()=>{props.handleDelete(cartItem)}} >Delete</button>
+
+                                <button className="btn btn-danger mt-4 mx-2 w-50" onClick={()=>{props.handleDelete(cartItem)}}>Delete</button>
                             </div>
                         </div>
                     </>
@@ -84,17 +93,15 @@ const Cart = (props) => {
                     
                 </div>
 
-                {/* Total and checkout */}
-                {/* {calculateTotal()}
-                {calculateTax()} */}
+
                 <div className="flex-column flex-grow-1 d-flex total-card m-5">
                     <div className="card p-5">
                         <h3 className="my-5">Order Summary</h3>
                         <h6 className="my-3">Total Items: ${priceTotal}</h6>
                         <h6 className="my-3">Shipping & handling: $12.00</h6>
-                        <h6 className="my-3">Subtotal: {priceTotal + 12}</h6>
-                        <h6 className="my-3">Estimated Tax: $?</h6>
-                        <h6 className="my-5">Order Total: {priceTotal + 12 + totalTax}</h6>
+                        <h6 className="my-3">Subtotal: ${priceTotal + 12}</h6>
+                        <h6 className="my-3">Estimated Tax: ${totalTax}</h6>
+                        <h6 className="my-5">Order Total: ${priceTotal + 12 + Number(totalTax)}</h6>
                         <input type="submit" onClick={checkOut} className="btn btn-info my-3" value="checkout"/>
                         
                         {showProcessing ? <h5 className="processing mx-5 my-4">processing</h5> : null}
